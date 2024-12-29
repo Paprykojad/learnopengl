695883cea1eaea73c406eb23adac4861f1b6ed92
paprykojad <paprykojad@gmail.com>
2024-12-30 00:14:36 +0100

    teraz mam kwadraty 3d i rotację wokół pierwszego

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include <glfw3.h>
 #include "Shader.h"
 #include <stb_image.h>
+#include <thread>
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
@@ -11,6 +12,7 @@ using namespace std;
 
 const uint windowWidth = 800;
 const uint windowHeight = 600;
+const uint framerate = 1;
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
     glViewport(0, 0, width, height);
@@ -38,6 +40,7 @@ public:
 };
 
 int main() {
+    const double frametime = 1.0 / (double)framerate;
     std::cout << "Hello, World!" << std::endl;
 
     glfwInit();
@@ -106,13 +109,6 @@ int main() {
         -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
         -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
     };
-    // float verticies[] = {
-    //     // positions          // colors           // texture coords
-    //     0.5f, 0.5f, 0.0f,       1.0f, 0.0f, 0.0f,    1.0f, 1.0f, // top right
-    //     0.5f, -0.5f, 0.0f,      0.0f, 1.0f, 0.0f,    1.0f, 0.0f, // bottom right
-    //     -0.5f, -0.5f, 0.0f,     0.0f, 0.0f, 1.0f,    0.0f, 0.0f, // bottom left
-    //     -0.5f, 0.5f, 0.0f,      1.0f, 1.0f, 0.0f,    0.0f, 1.0f // top left
-    // };
     unsigned int indicies[] = {
         0, 1, 2,
         0, 2, 3,
@@ -226,9 +222,30 @@ int main() {
 
     glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
     glEnable(GL_DEPTH_TEST);
+
+    glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
+    glm::vec3 cameraTarget = glm::vec3(0.0f, 0.0f, 0.0f);
+
+    glm::vec3 cameraDirection = glm::normalize(cameraPos - cameraTarget);
+    glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f);
+    glm::vec3 cameraRight = glm::normalize(glm::cross(up, cameraDirection));
+    glm::vec3 cameraUp = glm::cross(cameraDirection, cameraRight);
+
+    // glm::mat4 view;
+    view = glm::lookAt(glm::vec3(0.0f, 0.0f, 3.0f),
+                       glm::vec3(0.0f, 0.0f, 0.0f),
+                       glm::vec3(0.0f, 1.0f, 0.0f));
+
+
+
     class Time time;
     while(!glfwWindowShouldClose(window)) {
         processInput(window);
+
+        const float radius = 10.0f;
+        float camX = sin(glfwGetTime()) * radius;
+        float camZ = cos(glfwGetTime()) * radius;
+        view = glm::lookAt(glm::vec3(camX, 0.0, camZ), glm::vec3(0.0, 0.0, 0.0), glm::vec3(0.0, 1.0, 0.0));
 
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
@@ -265,6 +282,8 @@ int main() {
 
         glfwSwapBuffers(window);
         glfwPollEvents();
+
+        double dt = time.deltaTime();
     }
 
     glfwTerminate();
